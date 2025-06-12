a2eb30c2490ccabbe178067201d5fade9ea7b743
Udhay963 <b22ai061@kitsw.ac.in>
2025-06-12 22:50:28 +0530

    Update main.py

--- my_streamlit_app/main.py
+++ my_streamlit_app/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="📡 Telecom Project Launcher", layout="wide")
+st.set_page_config(page_title="📡 - Telecom Project Launcher", layout="wide")
 
 col1, col2 = st.columns(2)
 with col2:
